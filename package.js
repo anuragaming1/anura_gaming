6b8b89b8469f1372e60aaf0d8984119ddd5a6feb
Youtube: Anura Gaming <nhat75141@gmail.com>
2025-11-15 11:36:13 +0700

    Create package.js

---
+++ package.js
@@ -0,0 +1,18 @@
+{
+  "name": "anura-gaming",
+  "version": "1.0.0",
+  "type": "module",
+  "scripts": {
+    "dev": "next dev",
+    "build": "next build",
+    "start": "next start"
+  },
+  "dependencies": {
+    "next": "^13.0.0",
+    "react": "^18.0.0",
+    "react-dom": "^18.0.0",
+    "pg": "^8.11.3",
+    "bcryptjs": "^2.4.3",
+    "jsonwebtoken": "^9.0.2"
+  }
+}
